c8ad3e59e311137ef8aa2554f0570b221bb42dba
qmknvc <alem.mknvc@gmail.com>
2024-12-09 17:28:20 +0100

    Dodane dvije funkcije kvadriranje/modular

---
+++ index.js
@@ -0,0 +1,7 @@
+// Alem kvadriranje i modular (da li je djeljiv sa 2)
+function kvadriraj(brojZaKvadirati){
+    return brojZaKvadirati * 2
+}
+function modular(brojZaModular){
+    return brojZaModular % 2
+}
